8295e0c420687aa0c63acc43537902d14aa70e69
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2017-02-20 21:35:30 +0100

    Create metadata.php

---
+++ of2/monoxid/metadata.php
@@ -0,0 +1,36 @@
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+ 
+/**
+ * Module information
+ */
+$aModule = [
+    'id'           => 'of2_monoxid',
+    'title'        => 'Monoxid Logger',
+    'description'  => [
+        'de' => 'Logging Modul',
+        'en' => 'Module for Logging',
+    ],
+    'thumbnail'    => 'logo.jpg',
+    'version'      => '0.0.1',
+    'author'       => 'Keywan Ghadami',
+    'url'          => 'https://github.com/keywan-ghadami-oxid/monoxid',
+    'email'        => 'keywan.ghadami@oxid-esales.com',
+    'extend'       => [
+     
+         'oxutilsobject'                    => 'of2/monoxid/core/of2_monoxidoxutilsobject.php',
+    ],
+    'files'        => [
+         'Of2MonoxidLoggerFactory'          => 'of2/monoxid/core/Of2MonoxidLoggerFactory',
+    ], 
+    'events'       => [
+      
+    ],
+    'blocks' => [
+        
+    ],
+   'settings' => [
+    ]
+];
